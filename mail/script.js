9e9fc6af3330bdde9a817551495aeba4a817d5bf
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-05-03 16:13:16 +0200

    aggiungo codice js esercizio mail

---
+++ mail/script.js
@@ -0,0 +1,16 @@
+//  1. chiedo all'utente la sua mail
+const Mail = prompt("inserisci la tua mail");
+console.log(Mail);
+
+//  2. verifico se la sua mail e' presente sulla lista 
+const list = ["gino@gmail.com", "luca@gmail.com", "fabio@gmail.com"]
+
+//  3. stampiamo un esito della verifica
+for ( let i=0; i<list.length; i++){
+    if (list[i].includes(Mail)){
+        console.log("accedi");
+    } else{
+        console.log("mail non presente nella lista")
+    }
+    
+}
