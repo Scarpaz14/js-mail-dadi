2c9d494bafd14ce95b35f822de36f9215ffc521f
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-05-03 16:40:39 +0200

    sistemato

--- mail/script.js
+++ mail/script.js
@@ -6,7 +6,7 @@ console.log(Mail);
 const list = ["gino@gmail.com", "luca@gmail.com", "fabio@gmail.com"]
 
 //  3. stampiamo un esito della verifica
-let access= true;
+
 for ( let i=0; i<list.length; i++){
     if (list[i].includes(Mail)){
         console.log("accedi");
